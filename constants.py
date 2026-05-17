f1570c63ec58436887b465e32e5aefe10377c9ea
Maria Evdoshenko <evdoshenkomaria@yandex.ru>
2026-05-17 10:24:12 +0300

    refactor: вынести локаторы в locators.py, обновить константы в constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@
 
 BASE_URL = "https://qa-desk.education-services.ru"
 HOME_PAGE = BASE_URL
-REGISTRATION_PAGE_AFTER_SUCCESS = f"{BASE_URL}/regiatration"
+REGISTRATION_PAGE = f"{BASE_URL}/regiatration"
 LOGIN_PAGE_URL = BASE_URL + "/login"
-WAIT_TIMEOUT = 5
+WAIT_TIMEOUT = 10
--- locators.py
+++ locators.py
@@ -4,8 +4,10 @@ from selenium.webdriver.common.by import By
 class RegistrationPageLocators:
     """Локаторы элементов страницы регистрации"""
 
-    # Существующие локаторы
-    LOGIN_REGISTRATION_BUTTON = (By.CLASS_NAME, "buttonSecondary")
+    LOGIN_REGISTRATION_BUTTON = (
+        By.XPATH,
+        "//button[contains(text(), 'Вход и регистрация')]",
+    )
     NO_ACCOUNT_BUTTON = (By.XPATH, "//button[contains(., 'Нет аккаунта')]")
     EMAIL_FIELD = (By.NAME, "email")
     PASSWORD_FIELD = (By.NAME, "password")
@@ -35,8 +37,9 @@ class RegistrationPageLocators:
     )
     LOGOUT_BUTTON = (
         By.XPATH,
-        "//button[@class='spanGlobal btnSmall' and @type='button' and contains(text(), 'Выйти')]",
+        "//button[contains(@class, 'spanGlobal') and contains(@class, 'btnSmall') and @type='button' and normalize-space()='Выйти']",
     )
+
     LOGIN_SUBMIT_BUTTON = (
         By.XPATH,
         "//button[@type='submit' and contains(., 'Войти')]",
@@ -47,14 +50,25 @@ class RegistrationPageLocators:
         "//h1[contains(text(), 'Чтобы разместить объявление, авторизуйтесь')]",
     )
 
+    EMAIL_FIELD_ERROR_PARENT = (
+        By.XPATH,
+        "//div[contains(@class, 'input_inputError__fLUP9') and .//input[@name='email']]",
+    )
+    PASSWORD_FIELD_ERROR_PARENT = (
+        By.XPATH,
+        "//div[contains(@class, 'input_inputError__fLUP9') and .//input[@name='password']]",
+    )
+    CONFIRM_PASSWORD_FIELD_ERROR_PARENT = (
+        By.XPATH,
+        "//div[contains(@class, 'input_inputError__fLUP9') and .//input[@name='submitPassword']]",
+    )
+
 
 class AdCreationLocators:
-    """Локаторы элементов для создания объявления"""
 
     TITLE_FIELD = (By.CSS_SELECTOR, "input[name='name']")
     DESCRIPTION_FIELD = (By.CSS_SELECTOR, "textarea[name='description']")
     PRICE_FIELD = (By.CSS_SELECTOR, "input[name='price']")
-
     CONDITION_USED_LABEL = (
         By.XPATH,
         "//div[@class='createListing_inputRadio__ApFGD']//label[@class='h2' and text()='Б/У']",
@@ -71,25 +85,28 @@ class AdCreationLocators:
         By.XPATH,
         "//div[@class='createListing_inputRadio__ApFGD']//label[@class='h2' and text()='Новый']/preceding-sibling::div[contains(@class, 'inputActive')]",
     )
-
     SUBMIT_AD_BUTTON = (By.XPATH, "//button[contains(text(), 'Опубликовать')]")
-
-    
     MY_ADS_PAGE_TITLE = (By.XPATH, "//h1[contains(text(), 'Мои объявления')]")
     AD_IN_PROFILE_BY_TITLE = (
         By.XPATH,
         "//div[contains(@class, 'card')]//h2[contains(@class, 'h2') and contains(text(), '{}')]",
     )
-
     CATEGORY_DROPDOWN_BUTTON = (
         By.XPATH,
         "//input[@name='category']/following::button[contains(@class, 'dropDownMenu_arrowDown')][1]",
     )
-
     CITY_DROPDOWN_BUTTON = (
         By.XPATH,
         "//input[@name='city']/following::button[contains(@class, 'dropDownMenu_arrowDown')][1]",
     )
-
+    CITY_OPTIONS_IN_DROPDOWN = (
+        By.XPATH,
+        "//div[@class='dropDownMenu_optionsMobile__LmXZM']//button//span",
+    )
+    DROP_DOWN_MENU_OPTIONS = (By.CLASS_NAME, "dropDownMenu_optionsMobile__LmXZM")
+    CATEGORY_TECHNOLOGY_OPTION = (
+        By.XPATH,
+        "//div[@class='dropDownMenu_optionsMobile__LmXZM']//button//span[contains(text(), 'Технологии')]",
+    )
     CATEGORY_OPTIONS = (By.CSS_SELECTOR, ".category-option, .dropdown-item")
     CITY_OPTIONS = (By.CSS_SELECTOR, ".city-option, .dropdown-item")
